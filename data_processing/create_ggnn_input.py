72c717b9fb5ecc4727172049f5a1c86e0f40717c
EthanLubbock <elubbock@gmail.com>
2021-02-16 17:59:47 +0000

    Added create_ggnn_input

---
+++ data_processing/create_ggnn_input.py
@@ -0,0 +1,34 @@
+import os
+import json
+import argparse
+from tqdm import tqdm
+
+def raw_code2dict(file_path):
+    file_name = file_path.split('/')[-1]
+    output = {
+        'file_name':file_name,
+        'label':int(file_name[-3]),
+        'code':open(file_path, 'r').read()
+        }
+    return output
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project', help='project of code base from which to take raw code', default='chrome_debian')
+    parser.add_argument('--output', help='output directory for resulting json file', default='../data/ggnn_input/')
+    args = parser.parse_args()
+
+    code_file_path = '../data/' + args.project + '/raw_code/'
+
+    output_data = []
+    for cfile in tqdm(os.listdir(code_file_path)):
+        fp = code_file_path + cfile
+        output_data.append(raw_code2dict(fp))
+    
+    output_file = args.output + args.project + '_cfg_full_text_files.json'
+
+    with open(output_file, 'w') as of:
+        json.dump(output_data, of)
+        of.close()
+
+    print(f'Saved Output File to {output_file}')
