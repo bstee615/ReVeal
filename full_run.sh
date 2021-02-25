387872d9d52018ad1f91e9d07d430ffe4ee43398
EthanLubbock <elubbock@gmail.com>
2021-02-25 09:50:29 +0000

    changed paths in full_run.sh due to moving Devign repo

--- full_run.sh
+++ full_run.sh
@@ -15,7 +15,7 @@ mkdir -p "$outpdir/data/full_experiment_real_data/$name";
 mkdir -p "$outpdir/data/full_experiment_real_data_processed";
 mkdir -p "$outpdir/model";
 mkdir -p "$outpdir/ggnn_input";
-mkdir -p "$outpdir/after_ggnn";
+mkdir -p "$outpdir/after_ggnn/$name";
 
 #TODO: include Nick's slicer_all.sh to prep parsed directories for all c functions
 
@@ -29,6 +29,6 @@ python data_processing/full_data_prep_script.py --project "$name" --input "$inpd
 
 python data_processing/split_data.py --input "$outpdir/data/full_experiment_real_data_processed/$name-full_graph.json" --output "$outpdir/ggnn_input" --percent 50 --name "$name";
 
-python ../Devign/main.py --model_type ggnn --dataset "$name" --input_dir "$outpdir/ggnn_input/$name/" --feature_size 169;
+python Devign/main.py --model_type ggnn --dataset "$name" --input_dir "$outpdir/ggnn_input/$name/" --feature_size 169;
 
-python ../Devign/run_model.py --model "models/$name/GGNNSumModel-model.bin" --dataset "$outpdir/ggnn_input/processed.bin" --output_dir "$outpdir/after_ggnn" --name "$name";
+python Devign/run_model.py --model "models/$name/GGNNSumModel-model.bin" --dataset "$outpdir/ggnn_input/$name/processed.bin" --output_dir "$outpdir/after_ggnn" --name "$name";
