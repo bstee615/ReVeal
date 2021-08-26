2be98747ea92987d9825fa2e5a002fc41d00d907
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-08-26 16:09:59 -0500

    Move new_data_processing to data_processing

--- data_processing/create_ggnn_input.py
+++ data_processing/create_ggnn_input.py
@@ -1,35 +1,42 @@
-import os
 import json
-import argparse
-from tqdm import tqdm
+import logging
+
+import tqdm
+
+logger = logging.getLogger(__name__)
+
 
 def raw_code2dict(file_path):
     file_name = file_path.split('/')[-1]
     output = {
-        'file_name':file_name,
-        'label':int(file_name[-3]) if '_refactored' not in file_name else int(file_name[-(3+len('_refactored'))]),
-        'code':open(file_path, 'r').read()
-        }
+        'file_name': file_name,
+        'label': int(file_name[-3]) if '_refactored' not in file_name else int(file_name[-(3 + len('_refactored'))]),
+        'code': open(file_path, 'r').read()
+    }
     return output
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--project', help='name of project for differentiating files', default='chrome_debian')
-    parser.add_argument('--input', help='directory where raw code and parsed are stored', default='../data/chrome_debian')
-    parser.add_argument('--output', help='output directory for resulting json file', default='../data/ggnn_input/')
-    args = parser.parse_args()
 
-    code_file_path = args.input + '/raw_code/'
+def create_ggnn_input(code_dir, output_dir, project, store=False):
+    """
+    Collate JSON file of all
+    """
+
+    raw_code = code_dir / 'raw_code'
+    cfiles = raw_code.glob('*')
+    total = len(list(raw_code.glob('*')))
+    logger.info(f'{len(cfiles)} items')
 
     output_data = []
-    for cfile in tqdm(os.listdir(code_file_path)):
-        fp = code_file_path + cfile
-        output_data.append(raw_code2dict(fp))
-    
-    output_file = args.output + args.project + '_cfg_full_text_files.json'
+    for i, cfile in tqdm.tqdm(enumerate(cfiles)):
+        fp = raw_code / cfile
+        output = raw_code2dict(fp)
+        output["idx"] = i
+        output_data.append(output)
 
-    with open(output_file, 'w') as of:
-        json.dump(output_data, of)
-        of.close()
-
-    print(f'Saved Output File to {output_file}')
+    output_file = output_dir / (project + '_cfg_full_text_files.json')
+    if store:
+        with open(output_file, 'w') as of:
+            json.dump(output_data, of)
+            of.close()
+        logger.info(f'saved output File to {output_file}')
+    return total, output_data
