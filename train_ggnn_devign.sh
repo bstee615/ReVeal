85a4810dd8fd6709c9c9d1051661f5955b67bc8a
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-08-27 10:30:10 -0500

    Remove scripts

--- train_ggnn_devign.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sbatch batch.sh bash -x ggnn.sh -i "$PWD/data_refactored/chrome_debian" -o "$PWD/out_refactored/" -n chrome_debian
-sbatch batch.sh bash -x devign.sh -i "$PWD/data_refactored/chrome_debian" -o "$PWD/out_refactored/" -n chrome_debian
